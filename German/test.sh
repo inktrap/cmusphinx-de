6cb9c7c1e3ca93b99e57a4b6224737769407b9f1
inktrap <inktrap@inktrap.org>
2017-01-30 21:02:21 +0100

    test.sh

--- German/test.sh
+++ German/test.sh
@@ -2,3 +2,4 @@
 
 #pocketsphinx_continuous -inmic yes -lm cmusphinx-voxforge-de.lm -dict cmusphinx-voxforge-de.dic -hmm cmusphinx-de-voxforge-5.2/
 pocketsphinx_continuous -infile ../test.wav -lm cmusphinx-voxforge-de.lm.bin -dict cmusphinx-voxforge-de.dic -hmm cmusphinx-de-voxforge-5.2/
+
